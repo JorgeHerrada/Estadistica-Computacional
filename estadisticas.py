080de39320c67c65e352e2402ec6f9c251bba6cf
JorgeHerrada <jorge.herrada.28@gmail.com>
2022-07-03 18:04:40 -0500

    Now user can customize the lenght of the array to before getting the mean

--- estadisticas.py
+++ estadisticas.py
@@ -1,4 +1,6 @@
 import random
+from statistics import mean
+import statistics
 
 
 def media(X):
@@ -6,8 +8,19 @@ def media(X):
 
 
 if __name__ == '__main__':
-    X = [random.randint(1,21) for i in range(20)]
-    mu = media(X)
 
-    print(f'Lista de valores: {X}')
-    print(f'Media: {mu}')
+    # Get list lenght from user
+    n_elements = int(input("Ingresa el largo de la lista de elementos: "))
+
+    # list comprehension generation random elements for list within 1-20
+    X = [random.randint(1,21) for i in range(n_elements)]
+
+    # Calculate mean with custom function 
+    mu1 = media(X)
+    # Calculate mean with statistics library
+    mu2 = statistics.mean(X)
+
+    # Print results
+    print(f'Lista de valores({n_elements}): {X}')
+    print(f'Media con funcion: {mu1}')
+    print(f'Media con librería: {mu2}')
